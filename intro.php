631bc0c4cce78193b58b9abfd67cd3a0e705c4b9
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-09-26 01:39:35 +0800

    changes to json

--- intro.php
+++ intro.php
@@ -1,12 +1,6 @@
 
 <?php
-
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-
-$abc = "Hello";
+$abc = "Hello oink moo";
 $my_variable = 9;
 $var123 = 'c';
 $otherVar = true;
