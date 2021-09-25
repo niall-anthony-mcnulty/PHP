9d645d85b2a5fae96fe8526416d24f93e9f2aae5
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-09-25 23:59:16 +0800

    debug json

--- intro.php
+++ intro.php
@@ -1,8 +1,15 @@
+
 <?php
 
-echo "hello world";
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
 
-$a= 30;
-$b = 50;
-echo $a+$b;
+
+$abc = "Hello";
+$my_variable = 9;
+$var123 = 'c';
+$otherVar = true;
+
+echo $abc;
 ?>
