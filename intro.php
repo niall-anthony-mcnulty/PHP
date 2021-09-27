d09f9179204a2ceadff33c42586744c925a14c37
niall-anthony-mcnulty <niallanthony.mcnulty@gmail.com>
2021-09-28 00:02:05 +0800

    ;n

--- intro.php
+++ intro.php
@@ -1,9 +1,5 @@
-
-<?php
-$abc = "Hello oink moo";
-$my_variable = 9;
-$var123 = 'c';
-$otherVar = true;
-
-echo $abc;
-?>
+<form action='form.php'>
+      First Name: <input type='text' name='firstname' /><br>
+      Last Name: <input type='text' name='lastname' /><br>
+      <input type='submit' value='Submit' />
+</form>
